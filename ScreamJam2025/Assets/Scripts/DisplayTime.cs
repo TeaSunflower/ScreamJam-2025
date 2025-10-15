acb782d3cc2536d1c47348457f7f572f75417ec8
Mateusz Wargacki <mw4287@rit.edu>
2025-10-15 16:20:38 -0400

    Added scene transition

---
+++ ScreamJam2025/Assets/Scripts/DisplayTime.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayTime : MonoBehaviour
+{
+    [SerializeField]
+    Text display;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        display.text = "Time: " + ((int)Time.timeSinceLevelLoad).ToString();    
+    }
+}
